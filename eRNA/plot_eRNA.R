985f1b76213270a79f37a264a8ceecd07a6b98f6
Andrew Holding <andy-git@evath.net>
2018-10-25 19:31:00 +0100

    updated to new data

--- eRNA/plot_eRNA.R
+++ eRNA/plot_eRNA.R
@@ -12,19 +12,38 @@ oe_p <- oe_p + stat_compare_means(comparisons = oe_comparisons,
                        paired=TRUE)
 oe_p
 
-si<-eRNA[eRNA$Experiment=="knockdown",]
-
+si<-eRNA[eRNA$Experiment=="knockdown"& eRNA$Tissue=="MCF7",]
 si_comparisons <- list( c("siCtrl","siGRHL2") )
-p <- ggboxplot(si, x = "Treatment", y = "eRNA",
+MCF7p <- ggboxplot(si, x = "Treatment", y = "eRNA",
                add = "jitter", color="Treatment",
-               facet.by="Gene", ylab="Relative eRNA levels")
-p + stat_compare_means(comparisons = si_comparisons,
-                       method = "wilcox.test" ,
-                       paired = TRUE)
+                facet.by="Gene",ylab="Relative eRNA levels")
+MCF7p<-MCF7p + stat_compare_means(comparisons = si_comparisons,
+                       method = "t.test" ,label.y=0.0014,
+                       paired = TRUE)+theme(axis.title.x=element_blank())
+
+si<-eRNA[eRNA$Experiment=="knockdown"& eRNA$Tissue=="T47D",]
+si_comparisons <- list( c("siCtrl","siGRHL2") )
+T47Dp <- ggboxplot(si, x = "Treatment", y = "eRNA",
+               add = "jitter", color="Treatment",
+               facet.by="Gene",ylab="Relative eRNA levels")
+T47Dp<- T47Dp + stat_compare_means(comparisons = si_comparisons,
+                          method = "t.test" ,
+                          paired = TRUE,label.y=0.0005)+ guides(fill=FALSE, color=FALSE)+theme(axis.title.x=element_blank())
+
+si<-eRNA[eRNA$Experiment=="knockdown"& eRNA$Tissue=="ZR75",]
+si_comparisons <- list( c("siCtrl","siGRHL2") )
+ZR75p <- ggboxplot(si, x = "Treatment", y = "eRNA",
+                   add = "jitter", color="Treatment",
+                   facet.by="Gene",ylab="Relative eRNA levels")
+ZR75p<-ZR75p + stat_compare_means(comparisons = si_comparisons,
+                              method = "t.test" ,
+                              paired = TRUE,label.y=0.00025)+ guides(fill=FALSE, color=FALSE)
+si_p<-ggarrange(MCF7p,T47Dp,ZR75p,nrow=3,labels=c("MCF7","T47D","ZR75"))
+si_p
 
 #Figure for manuscript
 png("plot/eRNA.png",point=15)
-oe_p
+si_p
 dev.off()
 
 
